50be9017aa818ff8c652819a6ffcd679c82d25eb
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-18 16:44:26 +0800

    feat: initial login form module development

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './LoginForm.module.scss';
+
+interface LoginFormProps {
+  onLogin: (email: string, password: string) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
+  return (
+    <form className={styles.loginWrapper} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
