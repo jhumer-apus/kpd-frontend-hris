b6f734334fc2b6580c5d5527ce821ea443804128
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-18 16:44:40 +0800

    feat: initial register form module development

---
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './RegisterForm.scss';
+
+interface RegisterFormProps {
+  onRegister: (email: string, password: string) => void;
+}
+
+const RegisterForm: React.FC<RegisterFormProps> = ({ onRegister }) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onRegister(email, password);
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
