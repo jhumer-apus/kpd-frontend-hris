95b6d9f0b680096dcf2b84b71ff2911795e0cbf2
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-19 10:18:05 +0800

    feat: loginAPI Call error handling prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginRegisterPage from './pages/LoginRegisterPage/LoginRegisterPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <section>
+      <LoginRegisterPage/>
+      </section>
     </div>
   );
 }
