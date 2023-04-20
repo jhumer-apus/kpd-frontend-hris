e5c0e680d8b18218fa7e27507107f7bef03b9430
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-20 16:15:31 +0800

    feat: loading module updated

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -5,13 +5,18 @@ import styles from './Loading.module.scss';
 //import a spinning font awesome for loading  icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
+
+interface LoadingProps {
+  animateDown?: boolean;
+}
 
 
-// make the spinner spin using font awesome
-
-const Loading: React.FC = () => {
+const Loading: React.FC<LoadingProps> = ({ animateDown }) => {
   return (
-    <div className={styles.loading}>
+    // <div className={styles.loading}>
+    <div className={`${styles.loading} ${animateDown ? styles.animateDown : ''}`}>
+    {/* <div className={classNames(styles.loading, [{animateDown: styles.animateDown}])}> */}
         <FontAwesomeIcon icon={faSpinner} className={styles.spinner} />
         <h1 className={styles.message}>Loading... </h1>
     </div>
