5bbb6e3c95d6ec4d506380f3c786002c46b4cd3e
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-20 16:24:43 +0800

    feat: loginRegister page module development completion

--- src/pages/LoginRegisterPage/LoginRegisterPage.tsx
+++ src/pages/LoginRegisterPage/LoginRegisterPage.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+// import React from 'react';
+// import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import './LoginRegisterPage.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { userLogin } from '../../store/actions/auth';
 import { RootState } from '../../store/reducers';
+import style from './LoginRegisterPage.module.scss';
+import {  ReactComponent as LoginGirlSvg } from '../../special_assets/log-in-girl2.svg';
+import '../../special_assets/log-in-girl2.css';
 
 const LoginRegisterPage: React.FC = () => {
   const dispatch = useDispatch();
@@ -15,16 +18,21 @@ const LoginRegisterPage: React.FC = () => {
     dispatch(userLogin({ email, password, twoFactorToken}));
   };
 
-  const handleRegister = (email: string, password: string) => {
-    console.log('Registering with', email, password);
-    // Add your registration logic here
-  };
+  // const handleRegister = (email: string, password: string) => {
+  //   console.log('Registering with', email, password);
+  //   // Add your registration logic here
+  // };
 
   return (
-    <div className="login-register-page">
-      <LoginForm onLogin={handleLogin} error={error}/>
+    <main className={style.loginRegisterPage}>
+      <section className={style.loginGirlWrap}> 
+        <LoginGirlSvg />
+      </section>
+      <section className={style.loginFormWrap}>
+        <LoginForm onLogin={handleLogin} error={error}/>
+      </section>
       {/* <RegisterForm onRegister={handleRegister} /> */}
-    </div>
+    </main>
   );
 };
 
