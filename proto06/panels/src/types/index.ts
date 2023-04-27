2115d4a77bdaf13fa32b6b91598eb7bed38388d1
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-27 16:17:26 +0800

    fix: type errors for route and page

---
+++ proto06/panels/src/types/index.ts
@@ -0,0 +1,20 @@
+export * from "@/types/types-widgets/index";
+export * from "@/types/types-data/index";
+
+export interface IPage {
+    id: number;
+    icon: React.ReactNode;
+    name: string;
+    path: string;
+    element: React.ReactNode;
+    hasSubItems: boolean;
+    subItems?: Array<IPage> | undefined;
+}
+
+export interface IRoute {
+    title?: string;
+    id: number;
+    layout: string;
+    pages: Array<IPage>;
+}
+
