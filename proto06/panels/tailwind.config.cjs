17d83a695cba76c819fb599eeabeb498900f2b99
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-28 16:57:51 +0800

    feat: tailwind screen customized 1280px

--- proto06/panels/tailwind.config.cjs
+++ proto06/panels/tailwind.config.cjs
@@ -4,6 +4,9 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xl: "1280px",
+    },
     extend: {},
   },
   variants: {
