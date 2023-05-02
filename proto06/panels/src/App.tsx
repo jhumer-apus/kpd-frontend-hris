d62261029a121d8753a26a1a029bfc614275bdcf
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-02 16:59:34 +0800

    feat: app route fix for better routing

--- proto06/panels/src/App.tsx
+++ proto06/panels/src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
+import { Profile } from "./pages/dashboard";
+
 
 function App() {
+  const [ isAuthenticated, setIsAuthenticated ] = useState(true);
   return (
     <Routes>
-      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/" element={ isAuthenticated ? <Navigate to="/home"/> : <Auth />} />
+      <Route path="/home/*" element={ isAuthenticated ? <Dashboard/> : <Auth />} />
+      <Route path="/employee/*" element={<Dashboard />} />
+      {/* <Route path="/profile/*" element={<Profile />} /> */}
       <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
