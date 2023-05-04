6eb1522771ace3226033946acc2862feb274b378
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-04 17:04:49 +0800

    feat: data widgets folder re optimized

---
+++ proto06/panels/src/data/widgets-data/upcoming-birthday-data.ts
@@ -0,0 +1,94 @@
+import { CelebrantsDataProps } from "../../types";
+
+export const celebrantsData: Array<CelebrantsDataProps> = [
+    {
+        id: 1,
+        name: "John Doe",
+        daysToBirthday: 1,
+        department: "Marketing",
+        profilePic: "https://i.pravatar.cc/300?img=1",
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        daysToBirthday: 4,
+        department: "Sales",
+        profilePic: "https://i.pravatar.cc/300?img=2",
+    },
+    {
+        id: 3,
+        name: "Emily Brown",
+        daysToBirthday: 6,
+        department: "IT",
+        profilePic: "https://i.pravatar.cc/300?img=3",
+    },
+    {
+        id: 4,
+        name: "Michael Johnson",
+        daysToBirthday: 8,
+        department: "HR",
+        profilePic: "https://i.pravatar.cc/300?img=4",
+    },
+    {
+        id: 5,
+        name: "Sophia Davis",
+        daysToBirthday: 10,
+        department: "Marketing",
+        profilePic: "https://i.pravatar.cc/300?img=5",
+    },
+    {
+        id: 6,
+        name: "Matthew Blasco",
+        daysToBirthday: 13,
+        department: "Sales",
+        profilePic: "https://i.pravatar.cc/300?img=6",
+    },
+    {
+        id: 7,
+        name: "Esmon Torres",
+        daysToBirthday: 14,
+        department: "HR",
+        profilePic: "https://i.pravatar.cc/300?img=7",
+    },
+    {
+        id: 8,
+        name: "Matthew Blasco",
+        daysToBirthday: 15,
+        department: "IT",
+        profilePic: "https://i.pravatar.cc/300?img=8",
+    },
+    {
+        id: 9,
+        name: "Patricia Blasco",
+        daysToBirthday: 20,
+        department: "R&D",
+        profilePic: "https://i.pravatar.cc/300?img=9",
+    
+    },
+    {
+        id: 10,
+        name: "Jean Garcia",
+        daysToBirthday: 25,
+        department: "Marketing",
+        profilePic: "https://i.pravatar.cc/300?img=10",
+    
+    },
+    {
+        id: 11,
+        name: "Mr. Greenfield",
+        daysToBirthday: 26,
+        department: "Sales",
+        profilePic: "https://i.pravatar.cc/300?img=11",
+    },
+    {
+        id: 12,
+        name: "Ms. Greenfield",
+        daysToBirthday: 26,
+        department: "Housekeeping",
+        profilePic: "https://i.pravatar.cc/300?img=12",
+    },
+];
+
+export default {
+    celebrantsData
+};
