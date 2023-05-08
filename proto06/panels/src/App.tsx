e35cfed5157b0aad9b7e6a874ebc1e2a9128beec
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-08 16:52:17 +0800

    feat: add connected auth app.tsx

--- proto06/panels/src/App.tsx
+++ proto06/panels/src/App.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 import { Profile } from "./pages/dashboard";
 
+// Authentication Related Imports
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from "./store/reducers";
+import Cookies from 'js-cookie';
+import { userLoginSuccess } from "./store/actions/auth";
+
+
 
 function App() {
-  const [ isAuthenticated, setIsAuthenticated ] = useState(true);
+  const dispatch = useDispatch();
+  const { isAuthenticated, token } = useSelector((state: RootState) => state.auth);
+  console.log(useSelector((state: RootState) => state), "mwoooww");
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      dispatch(userLoginSuccess(token));
+    }
+    // setLoading(false);
+  }, [dispatch]);
+
   return (
     <Routes>
-      <Route path="/" element={ isAuthenticated ? <Navigate to="/home"/> : <Auth />} />
-      <Route path="/home/*" element={ isAuthenticated ? <Dashboard/> : <Auth />} />
+      <Route path="/" element={ isAuthenticated ? <Navigate to="/home"/> : <Auth/>} />
+      <Route path="/home/*" element={ isAuthenticated ? <Dashboard/> : <Auth/>} />
       <Route path="/employee/*" element={<Dashboard />} />
-      {/* <Route path="/profile/*" element={<Profile />} /> */}
-      <Route path="/auth/*" element={<Auth />} />
+      {/* <Route path="/auth/*" element={<Auth />} /> */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
