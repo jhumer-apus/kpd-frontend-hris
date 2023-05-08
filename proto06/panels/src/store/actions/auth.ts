b609305619326c355af56836fa8f890cd790e401
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-08 16:55:44 +0800

    feat: added user logout feature

--- proto06/panels/src/store/actions/auth.ts
+++ proto06/panels/src/store/actions/auth.ts
@@ -1,6 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 
 
-export const userLogin = createAction<{email: string, password: string, twoFactorToken?: string}>('USER_LOGIN');
+export const userLogin = createAction<{username: string, password: string, twoFactorToken?: string}>('USER_LOGIN');
 export const userLoginSuccess = createAction<string>('USER_LOGIN_SUCCESS');
 export const userLoginFailure = createAction<string>('USER_LOGIN_FAILURE');
+
+
+export const userLogout = createAction('USER_LOGOUT');
