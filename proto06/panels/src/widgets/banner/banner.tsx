7eb92349288e7b91d146460cae93826c5deb552d
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-15 17:00:59 +0800

    feat: added svg planet beside the current asvg

--- proto06/panels/src/widgets/banner/banner.tsx
+++ proto06/panels/src/widgets/banner/banner.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import styles from './custom-styles/Carousel.module.scss';
-
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/reducers";
 
 const CarouselUI = ({ items }: any) => {
+    const { employee_detail } = useSelector((state: RootState) => state.auth);
+
     const carouselItems = [
         {
           imageUrl: "/img/team-1.jpeg",
@@ -26,7 +29,7 @@ const CarouselUI = ({ items }: any) => {
         <div className={styles.exampleWrap}>
         <div className={styles.wholeCarouselWrap}>
             <div className={styles.leftSide}>
-                <div className={styles.greetingsBar}>Good Afternoon, User!</div>
+                <div className={styles.greetingsBar}>Good Afternoon, {employee_detail?.gender?.includes("F")? 'Ms.': 'Mr.'} {employee_detail?.last_name}!</div>
                 <div className={styles.newsWrap}>
                     <Carousel showThumbs={false} infiniteLoop useKeyboardArrows autoPlay swipeable={true} showStatus={false} showIndicators={false}>
                         {carouselItems.map((item, index) => (
@@ -49,10 +52,18 @@ const CarouselUI = ({ items }: any) => {
             </div>
             <div className={styles.rightSide}>
                 <iframe src={"/img/astronaut.svg"}/>
+                <svg className={styles.planet} viewBox="0 0 160 160" width="160" height="160">
+                    <circle cx="80" cy="80" r="50" fill="#757575" fill-opacity={"0.9"}/>
+                    <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
+                        <path d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z" fill="#fff">
+                        <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="1s" repeatCount="indefinite" />
+                        </path>
+                    </g>
+                    <path d="M 50,0 A 50,50 0 0,0 -50,0Z" transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)" fill="#757575" />
+                </svg>
             </div>
         </div>
         </div>
-
     </React.Fragment>
     
   );
