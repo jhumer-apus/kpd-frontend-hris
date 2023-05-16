53ecd5efdafbcc78d47f7457400cbf34a26c7baf
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-16 16:59:40 +0800

    feat: employee easy access data draft

---
+++ proto06/panels/src/data/employee-easy-access-data.ts
@@ -0,0 +1,94 @@
+import {
+  BellIcon,
+  PlusCircleIcon,
+  ShoppingCartIcon,
+  CreditCardIcon,
+  LockOpenIcon,
+  BanknotesIcon,
+  CircleStackIcon,
+  ArchiveBoxIcon,
+  AcademicCapIcon,
+  ClockIcon,
+  DocumentCheckIcon,
+  BellAlertIcon,
+  BuildingOffice2Icon,
+  FingerPrintIcon,
+  FolderOpenIcon,
+} from "@heroicons/react/24/solid";
+
+
+
+export const employeeEasyAccessData = [
+  {
+    icon: BellIcon,
+    color: "text-green-500",
+    title: "DTRS",
+    description: "View Summary",
+    redirection: "",
+  },
+  {
+    icon: ArchiveBoxIcon,
+    color: "text-red-500",
+    title: "UA",
+    description: "Apply Unaccounted Attendance",
+    redirection: "",
+  },
+  {
+    icon: AcademicCapIcon,
+    color: "text-blue-500",
+    title: "CAF",
+    description: "Call Approval",
+    redirection: "",
+  },
+  {
+    icon: ClockIcon,
+    color: "text-orange-500",
+    title: "Schedule",
+    description: "View Schedule",
+    redirection: "",
+  },
+  {
+    icon: DocumentCheckIcon,
+    color: "text-pink-500",
+    title: "Leaves",
+    description: "Apply Leaves",
+    redirection: "",
+  },
+  {
+    icon: BanknotesIcon,
+    color: "text-blue-gray-900",
+    title: "Loans",
+    description: "View Loans",
+    redirection: "",
+  },
+  {
+    icon: BellAlertIcon,
+    color: "text-red-500",
+    title: "HR Advisory",
+    description: "View HR Advisories",
+    redirection: "",
+  },
+  {
+    icon: BuildingOffice2Icon,
+    color: "text-blue-500",
+    title: "Overtime",
+    description: "Apply Overtime",
+    redirection: "",
+  },
+  {
+    icon: FingerPrintIcon,
+    color: "text-green-500",
+    title: "Biometrics",
+    description: "Biometrics Data",
+    redirection: "",
+  },
+  {
+    icon: FolderOpenIcon,
+    color: "text-blue-gray-500",
+    title: "Forms",
+    description: "Downloadables",
+    redirection: "",
+  },
+];
+
+export default employeeEasyAccessData;
