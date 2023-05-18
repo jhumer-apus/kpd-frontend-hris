1fa560629fcfe18b37f5b2ba09b67a6e8cd1acd4
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-18 16:56:31 +0800

    feat: employees reducer update to the root state

--- proto06/panels/src/store/reducers/index.ts
+++ proto06/panels/src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { authReducer } from './auth';
+import { employeesReducer } from './employees';
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  employees: employeesReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
