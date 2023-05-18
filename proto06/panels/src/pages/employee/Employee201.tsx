05a7b2ca007bf1bb2898658cf1658ed369d034ad
Mattttyyyy <wehttamocsalb@gmail.com>
2023-05-18 16:47:25 +0800

    feat: employee 201 file component connection to backend

--- proto06/panels/src/pages/employee/Employee201.tsx
+++ proto06/panels/src/pages/employee/Employee201.tsx
@@ -1,16 +1,21 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { useSelector, useDispatch } from 'react-redux';
+import { getEmployeesList } from '@/store/actions/employees';
+import { RootState } from '@/store/reducers';
+import { getSpecificEmployeeInfo } from '@/store/actions/employees';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
+  // { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'emp_no', headerName: 'Employee #', width: 120 },
+  { field: 'first_name', headerName: 'First Name', width: 150 },
+  { field: 'last_name', headerName: 'Last Name', width: 150 },
+  // {
+  //   field: 'age',
+  //   headerName: 'Age',
+  //   type: 'number',
+  //   width: 90,
+  // },
   {
     field: 'fullName',
     headerName: 'Full name',
@@ -18,35 +23,54 @@ const columns: GridColDef[] = [
     sortable: false,
     width: 160,
     valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      `${params.row.first_name || ''} ${params.row.last_name || ''}`,
   },
+  { field: 'date_hired', headerName: 'Date Hired', width: 150 },
+  { field: 'branch_code', headerName: 'Branch Code', width: 150 },
+  { field: 'mobile_phone', headerName: 'Mobile Number', width: 150 },
+  { field: `user`, headerName: 'Has HRIS Access', width: 150, valueGetter: (params: GridValueGetterParams) => `${params.row.user?.is_active ? 'Active' : 'No Access'}` },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
 export default function DataTable() {
+  const { employees_list, specific_employee_info } = useSelector((state: RootState) => state.employees);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getEmployeesList());
+  }, []);
+  const dispatchSpecificEmployeeInfo = (employee_number: number) => {
+    // console.log(typeof employee_number, "aahaha")
+    console.log(dispatch(getSpecificEmployeeInfo({employee_id: employee_number})), "haduken");
+    return dispatch(getSpecificEmployeeInfo({employee_id: employee_number}));   
+  }
+  console.log(specific_employee_info, "eto un tae", employees_list, "eto un list");
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 800, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={employees_list ?? []}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 25 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[25, 30]}
         checkboxSelection
+        onRowClick={(e) => {
+          console.log(e, dispatchSpecificEmployeeInfo(e.row.emp_no))
+        }}
+        style={{ cursor: 'pointer'}}
       />
     </div>
   );
