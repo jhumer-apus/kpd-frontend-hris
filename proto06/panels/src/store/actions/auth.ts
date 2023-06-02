5e2ae6345c5c67e9dc0dc51a483fee1fdd35f657
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-02 16:48:11 +0800

    feat: created new actions for data fetcher

--- proto06/panels/src/store/actions/auth.ts
+++ proto06/panels/src/store/actions/auth.ts
@@ -12,3 +12,12 @@ export const userLoginSuccess = createAction(
     "USER_LOGIN_SUCCESS",
     (jwt: string, user: UserType, employee_detail: EmployeeDetailsType) => ({ payload: { jwt, user, employee_detail } })
   );
+
+
+// New actions
+export const fetchUserData = createAction<{emp_no: Number}>('FETCH_USER_DATA');
+export const fetchUserDataSuccess = createAction(
+  'FETCH_USER_DATA_SUCCESS',
+  (employee_detail: EmployeeDetailsType) => ({ payload: { employee_detail } })
+  );
+export const fetchUserDataFailure = createAction('FETCH_USER_DATA_FAILURE');
