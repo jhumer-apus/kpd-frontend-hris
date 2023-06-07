bc5f92b249f58c0ba784674eef07fdb3b2ac5f02
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-07 16:57:58 +0800

    chore: commented out console.logs on codebase

--- proto06/panels/src/store/epics/employees.ts
+++ proto06/panels/src/store/epics/employees.ts
@@ -14,7 +14,7 @@ const getEmployeesListApiCall = async () => {
     // const response = await axios.post("https://bitverse-api.herokuapp.com/login", {
     // const response = await axios.post("http://172.16.168.144:8888/login", {
     const response = await axios.get("http://172.16.168.155:8000/api/employees");
-  console.log(response, "aaa");
+  // console.log(response, "aaa");
   return response.data;
 };
 
@@ -23,7 +23,7 @@ console.log(employee_id, "employee_id123")
   // const response = await axios.post("https://bitverse-api.herokuapp.com/login", {
   // const response = await axios.post("http://172.16.168.144:8888/login", {
 const response = await axios.get(`http://172.16.168.155:8000/api/employees/${employee_id}/`);
-console.log(response, "bbb");
+// console.log(response, "bbb");
 return response.data;
 };
 
@@ -40,7 +40,7 @@ export const employeesListEpic: Epic = (action$, state$) =>
           // Cookies.set('token', data.jwt, { expires: 1 / 24, secure: true });
           // Cookies.set('user', JSON.stringify(data.user), { expires: 1 / 24, secure: true });
           // Cookies.set('employee_detail', JSON.stringify(data.employee_detail), { expires: 1 / 24, secure: true });
-          console.log(data, "hallooooo11111");
+          // console.log(data, "hallooooo11111");
           return getEmployeesListSuccess(data);
         }),
         catchError((error) => {
@@ -63,16 +63,16 @@ export const employeesSpecificEpic: Epic = (action$, state$) =>
         getSpecificEmployeesInfoApiCall(action.payload.employee_id)
       ).pipe(
         map((data) => {
-          console.log("asdasdasdasd" , data)
+          // console.log("asdasdasdasd" , data)
           // Save the token in a secure cookie with an expiration time of 1 hour
           // Cookies.set('token', data.jwt, { expires: 1 / 24, secure: true });
           // Cookies.set('user', JSON.stringify(data.user), { expires: 1 / 24, secure: true });
           // Cookies.set('employee_detail', JSON.stringify(data.employee_detail), { expires: 1 / 24, secure: true });
-          console.log(data, "AKOOO ANG HARIIII");
+          // console.log(data, "AKOOO ANG Reynaaaaaa");
           return getSpecificEmployeeInfoSuccess(data);
         }),
         catchError((error) => {
-          console.log(error.response, "maeeeeee111owww");
+          // console.log(error.response, "maeeeeee111owww");
           if (error.response && error.response.data && error.response.data.error) {
             return of(getSpecificEmployeeInfoFailure(error.response.data.error)); // Extract error message from the response
           } else {
