88db116c8d9928bd0acbb1b4cc4aac0a477092b1
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-07 16:57:38 +0800

    feat: split-button development and data types declaration for dtr

---
+++ proto06/panels/src/widgets/split-button/split-button.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store/configureStore';
+import { saveIndexAndString } from '@/store/reducers/dtr';
+import customTheme from '@/customTheme';
+
+
+interface SplitButtonProps {
+  options?: Array<string>;
+}
+
+export default function SplitButton(props?:SplitButtonProps) {
+  const dispatch = useDispatch();
+  const viewDTRRecordsState = useSelector((state: RootState)=> state.dtr);
+  const defaultViewRecords = viewDTRRecordsState?.index || 0;
+  // console.log(magad, "checking")
+  const options = props?.options || ['Create a merge commit', 'Squash and merge', 'Rebase and merge'];
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(defaultViewRecords);
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+    dispatch(saveIndexAndString({index: selectedIndex, str: `${options[selectedIndex]}` }))
+    // console.log(magad, "checking default")
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number,
+  ) => {
+    // console.log( index, options[selectedIndex], "here?")
+    setSelectedIndex(index);
+    setOpen(false);
+    dispatch(saveIndexAndString({index: index, str: `${options[selectedIndex]}` }))
+    // console.log(magad, "checking toggle")
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <ButtonGroup variant="contained" color={"primary"} ref={anchorRef} aria-label="split button">
+        <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+        <Button
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          {/* To do: 76, 92, 193 default color override to primary*/}
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      // disabled={index === 2}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </React.Fragment>
+  );
+}
