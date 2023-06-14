fc1c052b3fcc007249006f4563b83943c0f160fb
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-14 14:56:08 +0800

    feat: upload dtr components code

---
+++ proto06/panels/src/pages/dtr/upload-dtr-logs/stepper/upload-dtr-component.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { Button } from '@mui/material';
+import { Typography } from '@material-tailwind/react';
+
+type ImportEmployee = {
+  file: any;
+};
+
+export const UploadDTRComponent = () => {
+
+    const [file, setFile] = useState<File | null>(null);
+    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFile(event.target.files ? event.target.files[0] : null);
+    };
+    const [editMode, setEditMode] = useState(true);
+    const onSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        if (!file) {
+          return;
+        }
+    
+        const formData = new FormData();
+        formData.append('file', file);
+    
+        try {
+          const response = await axios.post(
+            'http://172.16.168.155:8000/api/import_employee/',
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            }
+          );
+    
+          console.log(response.data);
+          setTimeout(() => {
+            location.reload();
+          }, 1000);
+        } catch (err) {
+          console.error(err);
+        }
+    };
+  return (
+    <form 
+        onSubmit={onSubmit} 
+        className="flex flex-col justify-center items-start h-52" 
+        style={{}}
+    >
+        <Typography
+            variant="small"
+            color="blue-gray"
+            className="mb-4 font-medium"
+        >
+            Upload TSV File of your DTR Logs
+        </Typography> 
+      <input type="file" accept=".tsv" onChange={onFileChange} />
+        {/* <Button variant="contained" color="primary" type="submit">
+            Submit
+        </Button> */}
+    </form>
+  );
+};
