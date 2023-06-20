88676b44bd1939a9cd27986c31109a37c9848bfa
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-20 17:02:34 +0800

    feat: optimize upload-dtr-component

--- proto06/panels/src/pages/dtr/upload-dtr-logs/local-components/upload-dtr-component.tsx
+++ proto06/panels/src/pages/dtr/upload-dtr-logs/local-components/upload-dtr-component.tsx
@@ -1,16 +1,10 @@
-import { ChangeEvent, SetStateAction, useState } from 'react';
-import axios from 'axios';
+import { ChangeEvent } from 'react';
 import { Button } from '@mui/material';
 import { Typography } from '@material-tailwind/react';
 import UploadFileIcon from "@mui/icons-material/UploadFile";
-import HelpIcon from '@mui/icons-material/Help';
 import TsvFileHelp from '../local-popovers/tsv-file-help';
 import { previewDtrCsvItem } from '@/types/types-pages';
 
-
-type ImportEmployee = {
-  file: any;
-};
 
 interface UploadDtrComponentProps {
   csvData?: previewDtrCsvItem[],
@@ -18,53 +12,16 @@ interface UploadDtrComponentProps {
   fileName?: string,
   setFileName?: (key: string) => void,
   handleFileUpload: (e: ChangeEvent<HTMLInputElement>) => void,
-  // setFile?: (e: SetStateAction<File | null>) => void, 
   onFileChange: (e: ChangeEvent<HTMLInputElement>) => void,
 }
 
 export const UploadDTRComponent = (props: UploadDtrComponentProps) => {
     const {handleFileUpload, onFileChange} = props;
 
-    // const [file, setFile] = useState<File | null>(null);
-    // const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setFile(event.target.files ? event.target.files[0] : null);
-    // };
-
-    
-    // const onSubmit = async (event: React.FormEvent) => {
-    //     event.preventDefault();
-    
-    //     if (!file) {
-    //       return;
-    //     }
-    
-    //     const formData = new FormData();
-    //     formData.append('file', file);
-    
-    //     try {
-    //       const response = await axios.post(
-    //         'http://172.16.168.155:8000/api/upload_dtr_logs/',
-    //         formData,
-    //         {
-    //           headers: {
-    //             'Content-Type': 'multipart/form-data',
-    //           },
-    //         }
-    //       );
-    
-    //       console.log(response.data);
-    //       // setTimeout(() => {
-    //       //   location.reload();
-    //       // }, 1000);
-    //     } catch (err) {
-    //       console.error(err);
-    //     }
-    // };
     const onFileChangeAndUpload = (e: ChangeEvent<HTMLInputElement>) => {
       onFileChange(e);
       handleFileUpload(e);
     };
-    // const [editMode, setEditMode] = useState(true);
   return (
     <div 
         className="flex flex-col justify-center items-start h-52" 
@@ -109,10 +66,6 @@ export const UploadDTRComponent = (props: UploadDtrComponentProps) => {
         >
             file by your Biometrics Machine
         </Typography> 
-      
-        {/* <Button variant="contained" color="primary" type="submit">
-            Submit
-        </Button> */}
     </div>
   );
 };
