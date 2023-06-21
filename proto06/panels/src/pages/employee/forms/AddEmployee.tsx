b64f2e703818e237db8401dc6002ef157992c7cf
Mattttyyyy <wehttamocsalb@gmail.com>
2023-06-21 16:44:54 +0800

    chore: cleanup logs

--- proto06/panels/src/pages/employee/forms/AddEmployee.tsx
+++ proto06/panels/src/pages/employee/forms/AddEmployee.tsx
@@ -1,64 +1,26 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
-import { TextField, Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { Button } from '@mui/material';
 import { Input, Typography } from '@material-tailwind/react';
-// import { TextField, Button } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
-// import { fetchUserData, updateUserData } from './userDataActions'; // import your actions
 import { getSpecificEmployeeInfo } from '@/store/actions/employees';
-import { RootState } from '@/store/configureStore';
 import { GetEmployeesListsType } from '@/types/types-store';
 
-
-type FormData = {
-  firstName: string;
-  lastName: string;
-  middleName: string;
-  age: number;
-  email: string;
-  password: string;
-};
-
 export const UserProfile = () => {
-    const [file, setFile] = useState<File | null>(null);
-    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFile(event.target.files ? event.target.files[0] : null);
-    };
     const { register, handleSubmit, setValue, formState: { errors } } = useForm<GetEmployeesListsType>();
     const dispatch = useDispatch();
-    const userData = useSelector((state: RootState) => state.employees.specific_employee_info);
     const [editMode, setEditMode] = useState(true);
-    // console.log(userData, "maasd")
 
     useEffect(() => {
         dispatch(getSpecificEmployeeInfo({employee_id: 33333}));
     }, [dispatch]);
 
-//   useEffect(() => {
-//     // update form values when userData changes
-//     for (const key in userData) {
-//       setValue(key as keyof FormData, userData.employees.specific_employee_info[key]  );
-//     }
-//   }, [userData, setValue]);
-
-useEffect(() => {
-    // update form values when userData changes
-    // if (userData) {
-    //   for (const key in userData) {
-    //     setValue(key as keyof FormData, userData[key]);
-    //   }
-    // }
-  }, [userData, setValue]);
-
   const onSubmit = async (data: GetEmployeesListsType) => {
     const formData = new FormData();
     for (const key in data) {
-        // console.log(key, "mamamiya")
         formData.append(key, data[key as keyof GetEmployeesListsType]);
     }
-
-    // console.log("formDatazzz: ", formData);
     try {
         const response = await axios.post(
           'http://172.16.168.155:8000/api/employees/',
@@ -79,7 +41,6 @@ useEffect(() => {
       }
     
       setEditMode(false);
-    // dispatch(updateUserData(data));
     setEditMode(false);
   };
 
