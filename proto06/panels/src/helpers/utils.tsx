ce0b2e40e0796a0971d0a8c64cb6da0f15a0a925
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-03 17:04:13 +0800

    feat: addtl helpers function to global

--- proto06/panels/src/helpers/utils.tsx
+++ proto06/panels/src/helpers/utils.tsx
@@ -19,3 +19,56 @@ export const flattenObject = (obj: any, prefix = ''): any => {
       return acc;
     }, {});
 }
+
+
+// totalDays represents the number of days you want to convert, considering 8 working hours per day. 
+// The function convertDaysToHHMM() is used to convert the days into the hh:mm format based on the 
+// assumption of 8 working hours per day. The resulting formatted time is then displayed 
+// in the component.
+
+export const convertDaysToHHMM = (days: number): string => {
+  const workingHoursPerDay = 8;
+  const totalMinutes = Math.floor(days * workingHoursPerDay * 60);
+
+  const hours = Math.floor(totalMinutes / 60);
+  const mins = totalMinutes % 60;
+
+  const hoursString = hours.toString().padStart(2, '0');
+  const minsString = mins.toString().padStart(2, '0');
+
+  return `${hoursString}:${minsString}`;
+}
+
+// This function takes the minutes as input and performs the following steps:
+// Divides the total minutes by 60 to get the hours.
+// Calculates the remaining minutes by taking the modulus (remainder) of the total minutes divided by 60.
+// Converts the hours and minutes into string format using toString().
+// Pads the hours and minutes strings with leading zeros using padStart() to ensure a consistent format.
+// Returns the formatted string in the hh:mm format.
+
+export const convertMinutesToHHMM = (minutes: number): string => {
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+
+  const hoursString = hours.toString().padStart(2, '0');
+  const minsString = mins.toString().padStart(2, '0');
+
+  return `${hoursString}:${minsString}`;
+}
+
+// If the truncated string is longer than the limit, 
+// the function searches for the last occurrence of a space character (' ') 
+// within the limit and removes everything after that.
+
+export const limitString = (str: string): string => {
+  const maxLength = 18;
+  let limitedString = str.substring(0, maxLength);
+
+  // Trim any trailing spaces if the string is longer than the limit
+  if (limitedString.length > maxLength) {
+    let lastSpaceIndex = limitedString.lastIndexOf(' ');
+    limitedString = limitedString.substring(0, lastSpaceIndex);
+  }
+
+  return limitedString;
+}
