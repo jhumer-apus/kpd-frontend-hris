5d63f61c88e026e1a49e3245fbd931d1d731e856
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-03 17:03:34 +0800

    feat: update useAuth rootstate

--- proto06/panels/src/custom-hooks/use-auth.ts
+++ proto06/panels/src/custom-hooks/use-auth.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Cookies from 'js-cookie';
-import { RootState } from '@/store/reducers';
+import { RootState } from '@/store/configureStore';
 import { userLoginSuccess, userLogout } from '@/store/actions/auth';
 
 export function useAuth() {
