bccd094ed5ef2bfcc69579cdeb603f3a2d7f4886
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-04 16:21:12 +0800

    feat: process payroll function initialized

--- proto06/panels/src/store/actions/payroll.ts
+++ proto06/panels/src/store/actions/payroll.ts
@@ -16,3 +16,22 @@ export const generatePayslipMultiple = createAction("GENERATE_PAYSLIP_MULTIPLE",
     return({ payload: data })});
 // export const viewPayrollListProgress = createAction<number>("VIEW_PAYROLL_LIST_PROGRESS");
 // export const viewPayrollListFailure = createAction<string>("VIEW_PAYROLL_LIST_FAILURE");
+
+
+// /* 
+
+// */
+// export const processPayroll = createAction("PROCESS_PAYROLL");
+// export const processPayrollSuccess = createAction("PROCESS_PAYROLL_SUCCESS", (data: number[]) => {
+// return ({payload: data})    
+// });                               
+// export const processPayrollProgress = createAction("PROCESS_PAYROLL_PROGRESS");
+// export const processPayrollFailure = createAction("PROCESS_PAYROLL_FAILURE");
+
+
+export const processPayroll = createAction<{emp_no: number[], cutoff_code: number}>("PROCESS_PAYROLL");
+export const processPayrollSuccess = createAction("PROCESS_PAYROLL_SUCCESS", (SuccessMessage: string) => { 
+    // console.log
+    return({ payload: {SuccessMessage} })});
+export const processPayrollProgress = createAction<number>('PROCESS_PAYROLL_PROGRESS');
+export const processPayrollFailure = createAction<string>("PROCESS_PAYROLL_FAILURE");
