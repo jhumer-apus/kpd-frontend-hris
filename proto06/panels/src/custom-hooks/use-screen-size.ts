d4346d981bb53d48611060ff9950b873a60202af
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-07 16:41:45 +0800

    feat: useScreen size custom hook devt

---
+++ proto06/panels/src/custom-hooks/use-screen-size.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+interface ScreenSize {
+  width: number;
+  height: number;
+}
+
+const useScreenSize = (): ScreenSize => {
+  const [screenSize, setScreenSize] = useState<ScreenSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = () => {
+    setScreenSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
+export default useScreenSize;
