7f0b42e22ebd00b2076c532e9f24fb76184d201c
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-13 17:07:51 +0800

    feat: configure interfaces for the new API reqs

--- proto06/panels/src/types/types-pages/index.ts
+++ proto06/panels/src/types/types-pages/index.ts
@@ -231,3 +231,163 @@ export type HolidayGetType = {
     holiday_type: string,
     holiday_location: HolidayLocationType,
 };
+
+
+export interface OBTCreateInterface {
+    obt_date_filed: string;
+    obt_type: string;
+    obt_location: string;
+    obt_remarks: string;
+    obt_date_from: string;
+    obt_date_to: string;
+    obt_approval_status: string;
+    obt_total_hour: number;
+    obt_approver1_empno: number | null;
+    obt_approver2_empno: number | null,
+    emp_no: number;
+    cutoff_code: number;
+}
+
+export interface OBTViewInterface extends OBTCreateInterface { 
+    id: number,
+    obt_reason_disapproval: string | null,
+    obt_date_approved1: string;
+    obt_date_approved2: string | null,
+}
+
+export interface OBTEditInterface extends OBTViewInterface {}
+
+
+export interface OVERTIMECreateInterface {
+    ot_date_filed: string;
+    ot_type: string;
+    ot_remarks: string;
+    ot_date_from: string;
+    ot_date_to: string;
+    ot_approval_status: string;
+    ot_reason_disapproval: string | null;
+    ot_total_hours: number;
+    ot_approver1_empno: number | null;
+    ot_approver2_empno: number | null;
+    emp_no: number;
+    cutoff_code: number;
+}
+
+export interface OVERTIMEViewInterface extends OVERTIMECreateInterface { 
+    id: number,
+    ot_reason_disapproval: string | null,
+    ot_date_approved1: string | null;
+    ot_date_approved2: string | null;
+}
+
+export interface OVERTIMEEditInterface extends OVERTIMEViewInterface {}
+
+
+export interface LEAVECreateInterface {
+    leave_date_filed: string;
+    leave_remarks: string;
+    leave_date_from: string;
+    leave_date_to: string;
+    leave_approval_status: string;
+    leave_total_hours: number;
+    leave_approver1_empno: number | null;
+    leave_approver2_empno: number | null;
+    leave_number_days: number;
+    emp_no: number;
+    cutoff_code: number;
+    leave_type: number;
+}
+
+export interface LEAVEViewInterface extends LEAVECreateInterface { 
+    id: number;
+    leave_reason_disapproval: string | null;
+    leave_date_approved1: string | null;
+    leave_date_approved2: string | null;
+}
+
+export interface LEAVEEditInterface extends LEAVEViewInterface {}
+
+
+
+
+export interface UACreateInterface {
+    ua_date_filed: string;
+    ua_description: string;
+    ua_date_from: string;
+    ua_date_to: string;
+    ua_approval_status: string;
+    ua_total_hour: number;
+    ua_approver1_empno: number | null;
+    ua_approver2_empno: number | null;
+    emp_no: number;
+    cutoff_code: number;
+}
+
+export interface UAViewInterface extends UACreateInterface { 
+    id: number;
+    ua_reason_disapproval: string | null;
+    ua_date_approved1: string | null;
+    ua_date_approved2: string | null;
+}
+
+export interface UAEditInterface extends UAViewInterface {}
+
+
+// Cases:
+
+// Case 1: Newly Created Leaves: PENDING
+// ua_approval_status: "PND"; ** default is Pending ** 
+// ua_approver1_empno: number | null; ** auto generated based on emp_profile ** 
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** 
+// ua_reason_disapproval: null; ** default is null - meaning newly created ** 
+// ua_date_approved1: string | null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+
+
+// Case 2: Denied Leaves: (if 1 approver)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to DSP due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string; ** if has remark, meaning, approver 1 denied ** Yes UI Frontend Edit Access **
+// ua_date_approved1: string | null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** remains null because - NO APPROVER2 ** YES UI Frontend Edit Access **
+
+// Case 3: Denied Leaves: (if 2 approver - approver 1 ignored? approver 2 denied)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to DSP due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string; ** if has remark, meaning, approver 1 ignored but approver 2 denied** Yes UI Frontend Edit Access **
+// ua_date_approved1: string | null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+
+// Case 4: Denied Leaves: (if 2 approver - approver 1 denied? approver 2 not necessary?)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to DSP due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string; ** if has remark, meaning, approver 1 denied and approver 2 dont need to change** Yes UI Frontend Edit Access **
+// ua_date_approved1: null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+// ua_date_approved2: null; ** remains null because - denied(IF HAS REMARK)/(OTHERWISE)pending ** YES UI Frontend Edit Access **
+
+// Case 5: Approved Leaves: (if 2 approver - approver 1 approved? approver 2 approved?)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to APD due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string | null; ** if NULL remark, meaning, approver 1 APPROVED and approver 2 ALSO APPROVED** Yes UI Frontend Edit Access **
+// ua_date_approved1: string | null; ** if has STRING - Meaning approver1 APPROVED ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** if has STRING - Meaning approver2 APPROVED ** YES UI Frontend Edit Access **
+
+// Case 6: Pending Leaves: (if 2 approver - approver 1 approved? approver 2 Pending?)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to APD due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string; ** if Null remark, meaning, approver 1 APPROVED and approver 2 is PENDING** Yes UI Frontend Edit Access **
+// ua_date_approved1: string | null; ** if has STRING - Meaning approver1 APPROVEDE ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** remains null because - approver2 denied(if has remarks)/(otherwise)pending ** YES UI Frontend Edit Access **
+
+// Case 7: Approved Leaves: (if 1 approver - approver 1 approved?)
+// ua_approval_status: "PND"; ** default is Pending ** No UI Frontend Edit Access ** Backend will change the status to APD due to remark **
+// ua_approver1_empno: number; ** auto generated based on emp_profile **  No UI Frontend Edit Access **
+// ua_approver2_empno: number | null; ** auto generated based on emp_profile ** No UI Frontend Edit Access **
+// ua_reason_disapproval: string; ** if NULL remark, meaning, approver 1 APPROVED and approver 2 is NULL** Yes UI Frontend Edit Access **
+// ua_date_approved1: string | null; ** if has STRING - Meaning approver1 APPROVED ** YES UI Frontend Edit Access **
+// ua_date_approved2: string | null; ** remains null because - NO APPROVER2 ** YES UI Frontend Edit Access **
