f7f070ed2800c416353d358269c5411ec8add511
Mattttyyyy <wehttamocsalb@gmail.com>
2023-07-31 11:52:53 +0800

    fix: fix the collapsible section not opening on the corresponding pathname

--- proto06/panels/src/widgets/layout/custom-effects/CollapsibleSection.tsx
+++ proto06/panels/src/widgets/layout/custom-effects/CollapsibleSection.tsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import styles from "./CollapsibleSection.module.scss";
+import { useLocation } from "react-router-dom";
 
 
 type CollapsibleSectionProps = {
@@ -10,14 +11,35 @@ type CollapsibleSectionProps = {
 
 export const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({ children, header, className }) => {
   const contentRef = useRef<HTMLDivElement>(null);
+  const location = useLocation(); // Get the current location object from react-router-dom
+  const [currentPath, setCurrentPath] = useState<string>(location.pathname);
+
   useEffect(() => {
-    // if (contentRef?.current?.getAttribute("data-collapsed") === "false") {
     if(contentRef.current) {
       contentRef.current.setAttribute("data-collapsed", "true");
       contentRef.current.style.height = "0px";
     }
   }, []);
 
+  useEffect(() => {
+    const checkDataLink = () => {
+      if (contentRef.current) {
+        const childrenNodes = contentRef.current.children;
+        if (childrenNodes) {
+          for (let i = 0; i < childrenNodes.length; i++) {
+            const childNode = childrenNodes[i] as HTMLElement;
+            const dataLink = childNode.getAttribute("data-link");
+            if (window.location.pathname.includes(dataLink || '')) {
+              expandSection(contentRef.current);
+            }
+          }
+        }
+      }
+    };
+
+    checkDataLink();
+    setCurrentPath(location.pathname);
+  }, [currentPath, window.location.pathname]);
 
   const collapseSection = (element: HTMLElement) => {
     const sectionHeight = element.scrollHeight;
@@ -56,10 +78,8 @@ const toggleCollapse = () => {
       const isCollapsed = contentRef.current.getAttribute('data-collapsed') === 'true';
       if (isCollapsed) {
         expandSection(contentRef.current);
-        contentRef.current.setAttribute('data-collapsed', 'false');
       } else {
         collapseSection(contentRef.current);
-        contentRef.current.setAttribute('data-collapsed', 'true');
       }
     }
   };
@@ -69,7 +89,7 @@ const toggleCollapse = () => {
         <div className={styles.sectionHeader} onClick={toggleCollapse}>
             {header}
         </div>
-        <div className={styles.sectionContent} ref={contentRef}>
+        <div className={styles.sectionContent} ref={contentRef} path-name="debug.child">
             {children}
         </div>
   </div>
