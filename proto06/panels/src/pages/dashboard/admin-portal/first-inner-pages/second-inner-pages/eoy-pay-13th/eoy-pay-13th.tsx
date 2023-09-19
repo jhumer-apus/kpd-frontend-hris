ac3c7b38d0a9ead4eae03fee07c56d11cce10afc
Mattttyyyy <wehttamocsalb@gmail.com>
2023-09-19 14:45:18 +0800

    feat: eoy-page-dashboard

---
+++ proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/eoy-pay-13th/eoy-pay-13th.tsx
@@ -0,0 +1,46 @@
+import { Fragment }from 'react';
+import { styled } from '@mui/material/styles';
+import MuiGrid from '@mui/material/Grid';
+import { Paper, Box, useTheme, useMediaQuery } from '@mui/material';
+import EOYPAY13THPageHistory from './local-component/right-side/eoy-pay-13th-history';
+import EOYPAY13THCreate from './local-component/left-side/eoy-pay-13th';
+
+
+const PaperStyle = {
+    padding: "20px",
+    height: "700px",
+    overflow: 'auto'
+}
+
+const Grid = styled(MuiGrid)(({ theme }) => ({
+    width: '100%',
+    height: "100%",
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      margin: theme.spacing(0, 2),
+    },
+}));
+
+export default function EOYPAY13TH() {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('lg'));
+
+  return (
+    <Fragment>
+        <Grid container direction={matches ? 'column' : 'row'} spacing={2}>
+            <Grid item xs>
+                <Paper elevation={3} style={PaperStyle}>
+                    <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+                        <EOYPAY13THCreate/>
+                    </Box>
+                </Paper>
+            </Grid>
+            <Grid item xs>
+                <Paper elevation={3} style={PaperStyle}>
+                    <EOYPAY13THPageHistory/>
+                </Paper>
+            </Grid>
+        </Grid>
+    </Fragment>
+  );
+}
---
+++ proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/eoy-pay-13th/local-component/left-side/eoy-pay-13th.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState, Dispatch, SetStateAction } from 'react';
+import { Button } from '@mui/material';
+import {TextField} from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store/configureStore';
+import EmployeeAutoComplete from './inner-ui-components/employee-autocomplete';
+import { Typography } from '@mui/joy';
+import { PAY13THCreateInterface } from '@/types/types-payroll-eoy';
+import { PAY13THCreateAction, PAY13THCreateActionFailureCleanup } from '@/store/actions/payroll-eoy';
+import CutoffAutoComplete from './inner-ui-components/cutoff-code-autocomplete';
+import BonusListAutoComplete from './inner-ui-components/bonus-type-autocomplete';
+import MultiEmployeeAutoCompleteLeft from './inner-ui-components/employee-autocomplete';
+
+
+import { DataGrid, GridCallbackDetails, GridRowSelectionModel } from '@mui/x-data-grid';
+// import { Typography } from "@material-tailwind/react";
+import { EOYPAY13THPageDescriptions, EOYPAY13THPageColumns, EOYProcessPAY13THPageColumns } from '@/data/pages-data/payroll-eoy-data/eoy-pay-13th-data';
+import { PAY13THViewInterface } from '@/types/types-payroll-eoy';
+import { PAY13THViewAction } from '@/store/actions/payroll-eoy';
+import { getEmployeesList } from '@/store/actions/employees';
+import { GetEmployeesListsType } from '@/types/types-store';
+
+
+
+interface CreatePAY13THModalInterface {
+    setOpen?: Dispatch<SetStateAction<boolean>>;
+}
+
+function EOYPAY13THCreate(props: CreatePAY13THModalInterface) {
+    const state = useSelector((state: RootState) => state.employees)
+    const dispatch = useDispatch();
+    const curr_user = useSelector((state: RootState)=> state.auth.employee_detail?.emp_no);
+    const PAY13THCreatestate = useSelector((state: RootState)=> state.payrollEOY.PAY13THCreate);
+    const [createPAY13TH, setCreatePAY13TH] = useState<PAY13THCreateInterface>({
+        emp_no: [],
+    });
+
+    console.log(createPAY13TH, "hahaha111", state.employees_list)
+    const onClickSubmit = () => {
+        dispatch(PAY13THCreateAction(createPAY13TH))
+    };
+
+    useEffect(()=> {
+        if(state.employees_list?.length === 0 || !state.employees_list ){
+            getEmployeesList()
+        }
+    }, [])
+    useEffect(()=> {
+        if(curr_user){
+            setCreatePAY13TH((prevState) => {
+                return (
+                    {
+                        ...prevState,
+                        current_user: curr_user
+                    }
+                )
+            })
+        }
+    }, [curr_user]) 
+
+    useEffect(()=>{
+        if(PAY13THCreatestate.status === 'succeeded'){
+            window.alert('Request Successful');
+            window.location.reload();
+        }else if(PAY13THCreatestate.status === 'failed'){
+            window.alert(`Request Failed, ${PAY13THCreatestate.error}`)
+            setTimeout(()=> {
+                dispatch(PAY13THCreateActionFailureCleanup());
+            }, 1000)
+        }
+    }, [PAY13THCreatestate.status])
+
+    const handleSelection = (newSelection: GridRowSelectionModel, details: GridCallbackDetails) => {
+        let emp_no_locale = [] as Array<number>;
+        newSelection.forEach((id) => {
+          const row = state.employees_list?.find((row) => row.id === id);
+          if (row) {
+            emp_no_locale.push(row.emp_no);
+          }
+        });
+    
+        setCreatePAY13TH((prevState) => ({
+          ...prevState,
+          emp_no: emp_no_locale,
+        }));
+      };
+
+    return (
+        <React.Fragment>
+            <Typography style={{border: '2px solid rgb(25, 118, 210)', width: '100%', textAlign: 'center', padding: '6px', background: 'rgb(245,247,248)', boxShadow: '4px 4px 10px rgb(200, 200, 222)'}} variant='plain' level="h6">Process 13th Month Pay Data</Typography>
+            <div className='flex flex-col gap-6 overflow-auto w-full'>
+                    <p>You can choose one or more employees to process their 13th month pay. Otherwise check all.</p>
+                    <div style={{ height: '450px', width: '100%' }}>
+                        <DataGrid
+                        rows={state.employees_list as GetEmployeesListsType[]}
+                        columns={EOYProcessPAY13THPageColumns}
+                        initialState={{
+                            pagination: {
+                            paginationModel: { page: 0, pageSize: 100 },
+                            },
+                        }}
+                        pageSizeOptions={[25, 50, 75, 100]}
+                        // onRowClick={(e) => {
+                        //     setSinglePAY13THDetailsData(e.row);
+                        //     setSinglePAY13THOpenModal(true);
+                        // }}
+                        onRowSelectionModelChange={handleSelection}
+                        checkboxSelection
+                        disableRowSelectionOnClick
+                        localeText={{ noRowsLabel: `Loading...` }}
+                        />
+                    </div>
+                <div className='flex justify-center mt-6' container-name='leave_buttons_container'>
+                    <div className='flex justify-between' style={{width:'100%'}} container-name='leave_buttons'>
+                        <Button variant='contained' onClick={onClickSubmit}>Process 13th Month Pay</Button>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default EOYPAY13THCreate;
+
