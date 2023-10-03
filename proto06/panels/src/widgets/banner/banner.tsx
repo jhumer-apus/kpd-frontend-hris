c958748b182f358f95fc9b6fdee5a5e29d98d2d1
Mattttyyyy <wehttamocsalb@gmail.com>
2023-10-03 17:53:00 +0800

    feat: home announcement development

--- proto06/panels/src/widgets/banner/banner.tsx
+++ proto06/panels/src/widgets/banner/banner.tsx
@@ -1,61 +1,112 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import styles from './custom-styles/Carousel.module.scss';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/configureStore";
 // import { RootState } from "@/store/reducers";
+import dayjs from "dayjs";
 import { UnderDevelopmentMsg } from "@/pages/dashboard/hris-portal/local-components/projects-card";
-import { ACTIVEANNOUNCEMENTViewAction } from "@/store/actions/payroll-eoy";
+import { ACTIVEANNOUNCEMENTViewAction, ANNOUNCEMENTViewAction } from "@/store/actions/payroll-eoy";
+import { APILink } from "@/store/configureStore";
+
 
 const CarouselUI = ({ items }: any) => {
     const dispatch = useDispatch();
     const { employee_detail } = useSelector((state: RootState) => state.auth);
     const state = useSelector((state: RootState) => state.payrollEOY.ACTIVEANNOUNCEMENTView)
+    const [greetings, setGreeting] = useState("");
+    
+
+    
     useEffect(()=> {
-        dispatch(ACTIVEANNOUNCEMENTViewAction({dept: 1, rank: 1, pin: false}))
+        let timeNow = new Date().getHours();
+        if (timeNow >= 5 && timeNow < 12){
+            setGreeting("Good Morning")
+        } else if (timeNow >= 12 && timeNow < 18){
+            setGreeting("Good Afternoon")
+        } else  {
+            setGreeting("Good Evening")
+        }
+        dispatch(ACTIVEANNOUNCEMENTViewAction({rank: 1, dept: 1, pin: true}))
     }, [])
-    console.log(state, "wawa?")
-    const carouselItems = [
+
+    const noAnnouncementsFound = [
         {
-          imageUrl: "/img/team-1.jpeg",
-          caption: "May 12, 2023",
+          imageUrl2: "/img/team-1.jpeg",
+          date: "",
           altText: "Image 1",
+          message: "There is no pinned announcement found. Check the announcement page/tab.",
+          posted_by: "Admin "
         },
         {
-          imageUrl: "/img/team-2.jpeg",
-          caption: "05/05/2023",
+          imageUrl2: "/img/team-2.jpeg",
+          date: "",
           altText: "Image 2",
+          message: "There is no pinned announcement found. Check the announcement page/tab.",
+          posted_by: "Admin "
         },
         {
-          imageUrl: "/img/team-3.jpeg",
-          caption: "05/02/2023",
+          imageUrl2: "/img/team-3.jpeg",
+          date: "",
           altText: "Image 3",
+          message: "There is no pinned announcement found. Check the announcement page/tab.",
+          posted_by: "Admin "
         },
       ];
+
+    const activeAnnouncement = state.data.map((item, index)=> {
+        return (
+            {
+                imageUrl: item.emp_image,
+                date: dayjs(item.date_posted).format("MMM DD, YYYY"),
+                altText: `profile-image${index}`,
+                message: item.message,
+                posted_by: item.emp_name
+                
+            }
+        )
+    })
   return (
     <React.Fragment>
         <div className={styles.exampleWrap}>
         <div className={styles.wholeCarouselWrap}>
             <div className={styles.leftSide}>
-                <div className={styles.greetingsBar}>Good Afternoon, {employee_detail?.gender?.includes("F")? 'Ms.': 'Mr.'} {employee_detail?.last_name}!</div>
-                <div className={styles.newsWrap}>
-                    <UnderDevelopmentMsg/>
+                <div className={styles.greetingsBar}>{greetings}, {employee_detail?.gender?.includes("F")? 'Ms.': 'Mr.'} {employee_detail?.last_name}!</div>
+                <div className={styles.newsWrap}> 
+                    {/* <span className="italic">Announcements</span> */}
+                    {/* <UnderDevelopmentMsg/> */}
                     <Carousel showThumbs={false} infiniteLoop useKeyboardArrows autoPlay swipeable={true} showStatus={false} showIndicators={false}>
-                        {carouselItems.map((item, index) => (
+                        {activeAnnouncement.length > 0 ?  
+                        activeAnnouncement.map((item, index) => (
                             <div className={styles.carouselWrap} key={index}>
                                 <div className={styles.date}>
-                                    {item.caption}
+                                    {item.date}
                                 </div>
                                 <div className={styles.caption}>
-                                    Lorem ipsum dolor sit amet consectetur dolor sit amet consectetur dolor sit amet consectetur
+                                    {item?.message}
                                 </div>
                                 <div className={styles.personInfo}>
-                                    <img className={styles.personPic} src={item.imageUrl} alt={item.altText} />
-                                    <div className={styles.person}>Esther Howard</div>
+                                    <img className={styles.personPic} src={`${APILink.replace('/api/v1/', '')}${item.imageUrl}`} alt={item.altText} />
+                                    <div className={styles.person}>{item?.posted_by}</div>
                                 </div>
                             </div>
-                            
-                        ))}
+                        ))
+                        : 
+                        noAnnouncementsFound.map((item, index) => (
+                            <div className={styles.carouselWrap} key={index}>
+                                <div className={styles.date}>
+                                    {item.date}
+                                </div>
+                                <div className={styles.caption}>
+                                    {item?.message}
+                                </div>
+                                <div className={styles.personInfo}>
+                                    <img className={styles.personPic} src={item.imageUrl2} alt={item.altText} />
+                                    <div className={styles.person}>{item?.posted_by}</div>
+                                </div>
+                            </div>
+                        ))
+                    }
                     </Carousel>
                 </div>
             </div>
