bdb883d17cb42ee1059306e179fc01798a44625e
Mattttyyyy <wehttamocsalb@gmail.com>
2023-10-04 13:19:09 +0800

    feat: api link update repackaged repo

--- proto06/panels/src/store/configureStore.ts
+++ proto06/panels/src/store/configureStore.ts
@@ -287,6 +287,6 @@ epicMiddleware.run(combineEpics(
 
 export type RootState = ReturnType<typeof rootReducer>;
 // export const APILink = 'http://18.141.159.149:8000/api/v1/';
-// export const APILink = 'https://mercovsk1.pythonanywhere.com/api/v1/';
-export const APILink = 'http://192.168.0.101:8000/api/v1/';
+export const APILink = 'https://mercovsk1.pythonanywhere.com/api/v1/';
+// export const APILink = 'http://192.168.0.101:8000/api/v1/';
 export default store;
