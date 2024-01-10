8fb4c42701b548f017dad9c6adff4274359a48bc
Mattttyyyy <wehttamocsalb@gmail.com>
2024-01-10 21:01:03 +0800

    feat: initial perfect attendance data

---
+++ proto06/panels/src/data/pages-data/home-data/perfect-attendance-data.tsx
@@ -0,0 +1,53 @@
+import { globalDate } from "@/store/configureStore";
+import { GridColDef, GridValueGetterParams, GridCellParams } from "@mui/x-data-grid";
+import dayjs from "dayjs";
+
+export const viewPayrollOptions = [
+  "View Payroll Per Employee",
+  "No Other Payroll View Options",
+];
+
+export const QuickAccessOBTPageDescriptions = [
+  "On this table, you will find the history of your filed requests for the past week, months, or years.",
+];
+
+
+export const QuickAccessOBTPageColumns: GridColDef[] = 
+[
+  {
+    field: 'obt_date_filed',
+    headerName: 'Date Filed',
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) => {
+      const date = new Date(params.row.obt_date_filed);
+      return dayjs(date).format(`${globalDate}`);
+    }
+  },
+  { field: 'emp_no', headerName: 'Filed By:', width: 120 },
+  { field: 'obt_approval_status', headerName: 'Status', width: 100,
+    renderCell: (params: GridCellParams) => {
+      const status = params.row?.obt_approval_status as string;
+
+      let cellColor = '';
+      if (status === 'P1' || status === 'P2') {
+        cellColor = '#ff9100'; // Orange
+      } else if ( status==='DIS' ){
+        cellColor = '#aa2e25'; // Red
+      }
+
+      return(
+        <div className='relative'>
+          <div style={{ top:'', left: '26px', position: 'absolute', backgroundColor: cellColor, height:'5px', width: '5px', borderRadius: '100px'}}></div>
+          {status}
+        </div>
+      );
+    }  
+  },
+  { field: 'obt_reason_disapproval', headerName: 'Disapproval Reason',  width: 200 },
+];
+  
+export default {
+  QuickAccessOBTPageDescriptions,
+  QuickAccessOBTPageColumns
+};
+  
