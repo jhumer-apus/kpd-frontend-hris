b41f251e746dd4393e243fb0618c665ff39851e9
Mattttyyyy <wehttamocsalb@gmail.com>
2024-02-05 23:21:51 +0800

    fix: typo errors

--- proto06/panels/src/pages/procedurals/cutoff-periods/local-component/left-side/procedural-cutoff-period-create.tsx
+++ proto06/panels/src/pages/procedurals/cutoff-periods/local-component/left-side/procedural-cutoff-period-create.tsx
@@ -44,7 +44,7 @@ function ProceduralCUTOFFPERIODCreate(props: CreateCUTOFFPERIODModalInterface) {
 
     return (
         <React.Fragment>
-            <Typography style={{border: '2px solid rgb(25, 118, 210)', width: '100%', textAlign: 'center', padding: '6px', background: 'rgb(245,247,248)', boxShadow: '4px 4px 10px rgb(200, 200, 222)'}} variant='plain'>Create CUTOFFPERIOD Individual Data</Typography>
+            <Typography style={{border: '2px solid rgb(25, 118, 210)', width: '100%', textAlign: 'center', padding: '6px', background: 'rgb(245,247,248)', boxShadow: '4px 4px 10px rgb(200, 200, 222)'}} variant='plain'>Create CUTOFF PERIOD Individual Data</Typography>
             <div className='flex flex-col gap-6 overflow-auto relative'>
                 <div className='flex gap-6 pt-4'>
                     <div className='flex flex-col gap-6'>
@@ -151,7 +151,7 @@ function ProceduralCUTOFFPERIODCreate(props: CreateCUTOFFPERIODModalInterface) {
                 </div>
                 <div className='flex justify-center mt-6' container-name='leave_buttons_container'>
                     <div className='flex justify-between' style={{width:'100%'}} container-name='leave_buttons'>
-                        <Button variant='contained' onClick={onClickSubmit}>Create CUTOFFPERIOD</Button>
+                        <Button variant='contained' onClick={onClickSubmit}>Create CUTOFF PERIOD</Button>
                     </div>
                 </div>
             </div>
