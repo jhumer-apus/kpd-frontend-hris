74637c9f4a336b5e72dbbafd4e2dce1901a49a18
jhumer-apus <jhumerapus@gmail.com>
2024-02-09 11:45:38 +0800

    view skyfreight reports updated

--- proto06/panels/src/pages/reports/view-skyfreight-reports/view-skyfreight-reports.tsx
+++ proto06/panels/src/pages/reports/view-skyfreight-reports/view-skyfreight-reports.tsx
@@ -11,29 +11,29 @@ import { globalServerErrorMsg } from '@/store/configureStore';
 interface EmployeeData {
     id: number;
     emp_no: number;
-    emp_full_name: string;
+    full_name: string;
     [date: string]: string | number;
 }
 export default function ViewSkyFreightReports() {
 
     const [isLoading, setIsLoading] = useState<Boolean>(false);
-    // const [rows, setRows] = useState<any[]>([]);
+    
     // dont need the local state because, better to declare the data coming from API into the central state?
     const [columnsApi, setColumnsApi] = useState<[]>([])
+    const [dateColumns, setDateColumns] = useState<GridColDef []>([]);
+    const [rows, setRows] = useState<EmployeeData[]>([]);
 
-    useEffect(()=> {
-        //set dispatch action here to get response from Marc's API with query of whatever month / year to check report
-        //code here
-        //setState setColumnsApi to list down all month of the queried request 
-        //code here
+    // useEffect(()=> {
+    //     //set dispatch action here to get response from Marc's API with query of whatever month / year to check report
+    //     //code here
+    //     //setState setColumnsApi to list down all month of the queried request 
+    //     //code here
 
-        //dont forget to set the dependencies of useEffect
-    }, [])
+    //     //dont forget to set the dependencies of useEffect
+    // }, [])
 
     const [month, setMonth] = useState<number>(10);
     const [year, setYear] = useState<number>(2023);
-
-    const [listOfCurrentMonth, setListOfCurrentMonth] = useState<String[]>([]);
 
     const getLastDayOfMonth = (year: number, month: number) => {
         
@@ -49,9 +49,10 @@ export default function ViewSkyFreightReports() {
         for(let i = 1; i <= lastDay; i++) {
             const date = new Date(year, month - 1, i);
 
+            const convertedDateToLocaleString = convertDateToLocalString(date);
             const column = { 
-                field: 'schedule' + i, 
-                headerName: date.toLocaleDateString(), 
+                field: convertedDateToLocaleString, 
+                headerName: convertedDateToLocaleString, 
                 width: 150 
             }
             listOfDaysInMonth.push(column)
@@ -59,109 +60,79 @@ export default function ViewSkyFreightReports() {
         return listOfDaysInMonth;
     }
 
-    const columnGrid = getColumnsDailyOnSpecificMonth(year, month);
-
-    // const getSkyFreightReports = async () => {
-
-    //     setIsLoading(true);
-
-    //     await axios.get(`${APILink}schedule_daily/?month=${month}&year=${year}`).then(response => {
-
-    //         const data = response.data;
-
-    //         columnGrid.forEach(date => {
-
-    //             data.forEach((employee:any) => {
-
-    //                 const foundEmployee = rows.find(emp => emp.emp_no == employee.emp_no)
+    const convertTimeToAMPMFormat = (timeString:string): string => {
+        // Split the time string into hours, minutes, and seconds
+        const [hours, minutes, seconds] = timeString.split(':');
     
-    //                 if(foundEmployee){
+        // Convert hours to AM/PM format
+        let formattedHours = parseInt(hours, 10) % 12;
+        formattedHours = formattedHours === 0 ? 12 : formattedHours;
     
-    //                 } else {
+        // Add leading zeros to minutes and seconds
+        const formattedMinutes = minutes.padStart(2, '0');
+        const formattedSeconds = seconds.padStart(2, '0');
     
-    //                     const empScheduleData = {
-    //                         id: employee.id,
-    //                         full_name: employee.full_name,
-    //                     }
-                        
-    //                     if(date == convertDateToLocalString(employee.business_date)) {
-
-    //                     }
-    //                     rows.push()
-    //                 }
+        // Determine whether it's AM or PM
+        const amOrPm = parseInt(hours, 10) < 12 ? 'AM' : 'PM';
     
-    //             });
+        // Construct the formatted time string
+        return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${amOrPm}`;
+    };
 
-    //         })
-    
-    //         // data.forEach((employee:any) => {
+    const getSkyFreightReports = async () => {
 
-    //         //     const foundEmployee = rows.find(emp => emp.emp_no == employee.emp_no)
+        setIsLoading(true);
 
-    //         //     if(foundEmployee){
+        await axios.get(`${APILink}schedule_daily/?month=${month}&year=${year}`).then(response => {
 
-    //         //     } else {
+            const data = response.data;
 
-    //         //         const empScheduleData = {
-    //         //             id: employee.id,
-    //         //             full_name: employee.full_name,
-    //         //         }
+            data.forEach((emp_orig:any) => {
 
-    //         //         columnGrid.forEach(date => {
-    //         //             if()
-    //         //         })
-    //         //         rows.push()
-    //         //     }
+                const foundEmployeeIndex = rows.findIndex(emp_strut => {
+                    return emp_strut.emp_no == emp_orig.emp_no
+                });
 
-    //         // });
+                if(foundEmployeeIndex > -1) {
+
+                    const dateKey = convertDateToLocalString(emp_orig.business_date);
+                    rows[foundEmployeeIndex][dateKey] = convertTimeToAMPMFormat(emp_orig.schedule_shift_code?.time_in) + "-" + convertTimeToAMPMFormat(emp_orig.schedule_shift_code?.time_out)
+
+                } else {
+                    const dateKey = convertDateToLocalString(emp_orig.business_date);
+                    
+                    let employee: EmployeeData = {
+                        id: emp_orig.id,
+                        full_name: emp_orig.full_name,
+                        emp_no: emp_orig.emp_no,
+                    } 
+                    employee[dateKey] = convertTimeToAMPMFormat(emp_orig.schedule_shift_code?.time_in) + "-" + convertTimeToAMPMFormat(emp_orig.schedule_shift_code?.time_out);
+                    rows.push(employee);
+                }
+            })
+            
 
             
-    //         // for (let i = 0; i < columnGrid.length; i++) {
 
-    //         //     const row:any = {}
-    //         //     const key = columnGrid[i];
+            setIsLoading(false)
+        })
 
-    //         //     data.forEach((employee:any) => {
+    }
 
-    //         //         const businessDate = convertDateToLocalString(employee.businessDate);
-    //         //         if(key == businessDate) {
+    const convertDateToLocalString = (date: Date) => {
 
-    //         //         }
+        const dateObject = new Date(date);
 
-    //         //     })
+        const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-based
+        const day = String(dateObject.getDate()).padStart(2, '0');
+        const year = dateObject.getFullYear();
 
-    //         //     if(key) {
+        const formattedDate = `${month}/${day}/${year}`;
 
-    //         //     }
+        console.log(formattedDate);
+        return formattedDate;
+    }
 
-    //         //     row[key] = 'value'; 
-    //         // }
-
-    //         // setRows(currentRows => response.data);
-    //         setIsLoading(false)
-    //     })
-
-    // }
-
-    // const convertDateToLocalString = (date: Date) => {
-
-    //     const dateObject = new Date(date);
-
-    //     const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-based
-    //     const day = String(dateObject.getDate()).padStart(2, '0');
-    //     const year = dateObject.getFullYear();
-
-    //     const formattedDate = `${month}/${day}/${year}`;
-
-    //     console.log(formattedDate);
-    //     return formattedDate;
-    // }
-
-
-
-    // useEffect(() => {
-
-    // },);
 
     // const organizeDailySchedulesOfemployees = () => {};
 
@@ -179,66 +150,69 @@ export default function ViewSkyFreightReports() {
     //     console.log(rows);
     // }, [rows])
 
+    useEffect(() => {
+        setDateColumns(() => getColumnsDailyOnSpecificMonth(year, month));
+        getSkyFreightReports();
+        // setRows([
+        //     {
+        //       id: 1,
+        //       emp_no: 1001,
+        //       emp_full_name: "john_doe1",
+        //       "10/01/2023": "7:00AM-5:45PM",
+        //       "10/02/2023": "",
+        //       "10/03/2023": "7:00AM-5:45PM",
+        //       "10/04/2023": "7:00AM-5:45PM",
+        //     },
+        //     {
+        //       id: 2,
+        //       emp_no: 1002,
+        //       emp_full_name: "john_doe2",
+        //       "10/01/2023": "7:00AM-5:45PM",
+        //       "10/02/2023": "",
+        //       "10/03/2023": "7:00AM-5:45PM",
+        //       "10/04/2023": "7:00AM-5:45PM",
+        //     },
+        //     {
+        //       id: 3,
+        //       emp_no: 1003,
+        //       emp_full_name: "john_doe3",
+        //       "10/01/2023": "7:00AM-5:45PM",
+        //       "10/02/2023": "",
+        //       "10/03/2023": "7:00AM-5:45PM",
+        //       "10/04/2023": "7:00AM-5:45PM",
+    
+        //     },
+        //     {
+        //       id: 4,
+        //       emp_no: 1004,
+        //       emp_full_name: "john_doe4",
+        //       "10/01/2023": "7:00AM-5:45PM",
+        //       "10/02/2023": "",
+        //       "10/03/2023": "7:00AM-5:45PM",
+        //       "10/04/2023": "7:00AM-5:45PM",
+    
+        //     }
+        //   ]);
+    }, [])
 
-    const rows: EmployeeData[] = [
-        {
-          id: 1,
-          emp_no: 1001,
-          emp_full_name: "john_doe1",
-          "10/01/2023": "7:00AM-5:45PM",
-          "10/02/2023": "",
-          "10/03/2023": "7:00AM-5:45PM",
-          "10/04/2023": "7:00AM-5:45PM",
-        },
-        {
-          id: 2,
-          emp_no: 1002,
-          emp_full_name: "john_doe2",
-          "10/01/2023": "7:00AM-5:45PM",
-          "10/02/2023": "",
-          "10/03/2023": "7:00AM-5:45PM",
-          "10/04/2023": "7:00AM-5:45PM",
-        },
-        {
-          id: 3,
-          emp_no: 1003,
-          emp_full_name: "john_doe3",
-          "10/04/2023": "7:00AM-5:45PM",
-          "10/01/2023": "7:00AM-5:45PM",
-          "10/02/2023": "",
-          "10/03/2023": "7:00AM-5:45PM",
-
-        },
-        {
-          id: 4,
-          emp_no: 1004,
-          emp_full_name: "john_doe4",
-          "10/04/2023": "7:00AM-5:45PM",
-          "10/01/2023": "7:00AM-5:45PM",
-          "10/02/2023": "",
-          "10/03/2023": "7:00AM-5:45PM",
-
-        }
-      ];
 
 
     // Extracting all unique date keys from the rows
-    const allDates = rows.reduce((dates, row) => {
-        Object.keys(row).forEach(key => {
-        if (key !== 'id' && key !== 'emp_no' && key !== 'emp_full_name' && !dates.includes(key)) {
-            dates.push(key);
-        }
-        });
-        return dates;
-    }, [] as string[]);
-    console.log(allDates)
+    // const allDates = rows.reduce((dates, row) => {
+    //     Object.keys(row).forEach(key => {
+    //     if (key !== 'id' && key !== 'emp_no' && key !== 'emp_full_name' && !dates.includes(key)) {
+    //         dates.push(key);
+    //     }
+    //     });
+    //     return dates;
+    // }, [] as string[]);
       
-    // Generating column definitions for each date
-    const dateColumns = allDates.map(date => ({
-        field: date,
-        headerName: date,
-        width: 150,
-    }));
+    // // Generating column definitions for each date
+    // const dateColumns = allDates.map(date => ({
+    //     field: date,
+    //     headerName: date,
+    //     width: 150,
+    // }));
     
     const columns: GridColDef[] = [
         { 
