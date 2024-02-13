65fe198d4a4deeac5f13c287f1470a60e60193c7
Mattttyyyy <wehttamocsalb@gmail.com>
2024-02-13 21:22:53 +0800

    fix: new apilink for pythonanywhere

--- proto06/panels/src/store/configureStore.ts
+++ proto06/panels/src/store/configureStore.ts
@@ -351,7 +351,8 @@ epicMiddleware.run(combineEpics(
 
 export type RootState = ReturnType<typeof rootReducer>;
 // export const APILink = 'http://18.141.159.149:8000/api/v1/';
-export const APILink = 'https://mercovsk1.pythonanywhere.com/api/v1/';
+// export const APILink = 'https://mercovsk1.pythonanywhere.com/api/v1/';
+export const APILink = 'https://bitversecorporation.pythonanywhere.com/api/v1/';
 // export const APILink = 'http://192.168.1.249:8000/api/v1/';
 // export const APILink = 'http://192.168.0.101:8000/api/v1/';
 export const JSONServer = 'http://localhost:3030/';
