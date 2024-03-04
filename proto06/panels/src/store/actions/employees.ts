2043db0ceb8613596af2abea6b03f40e0f65e3b8
Mattttyyyy <wehttamocsalb@gmail.com>
2024-03-04 23:05:40 +0800

    feat: add application on employee action reducer optimized

--- proto06/panels/src/store/actions/employees.ts
+++ proto06/panels/src/store/actions/employees.ts
@@ -6,8 +6,10 @@ export const getEmployeesList = createAction("GET_EMPLOYEES");
 export const getEmployeesListSuccess = createAction("GET_EMPLOYEES_LIST_SUCCESS", (list: Array<_Type.EMPLOYEESViewInterface>) => { 
     return({ payload: {list} })});
 export const getEmployeesListFailure = createAction<string>("GET_EMPLOYEES_LIST_FAILURE");
+export const getEmployeesListFailureCleanup = createAction("GET_EMPLOYEES_LIST_FAILURE_CLEANUP");
 
 
 export const getSpecificEmployeeInfo = createAction<{employee_id: number}>("GET_SPECIFIC_EMPLOYEE_INFO");
 export const getSpecificEmployeeInfoSuccess = createAction("GET_SPECIFIC_EMPLOYEE_INFO_SUCCESS", (list: _Type.EMPLOYEESViewInterface) => ({ payload: {list} }));
 export const getSpecificEmployeeInfoFailure = createAction<string>("GET_SPECIFIC_EMPLOYEE_INFO_FAILURE");
+export const getSpecificEmployeeInfoFailureCleanup = createAction<string>("GET_SPECIFIC_EMPLOYEE_INFO_FAILURE_CLEANUP");
--- proto06/panels/src/store/reducers/employees.ts
+++ proto06/panels/src/store/reducers/employees.ts
@@ -3,19 +3,24 @@ import { EMPLOYEESViewInterface } from '@/types/types-store';
 import { 
   getSpecificEmployeeInfo,
   getSpecificEmployeeInfoSuccess, 
-  getSpecificEmployeeInfoFailure, 
+  getSpecificEmployeeInfoFailure,
+  getSpecificEmployeeInfoFailureCleanup, 
   getEmployeesListFailure, 
+  getEmployeesListFailureCleanup, 
   getEmployeesListSuccess 
 } from '../actions/employees';
+import { globalReducerFailed, globalReducerLoading, globalReducerRefreshed, globalReducerSuccess } from '../configureStore';
 
 
 interface AuthState {
+  status: 'succeeded' | 'loading' | 'refreshed' | 'failed' | ''
   employees_list: EMPLOYEESViewInterface[];
   specific_employee_info: EMPLOYEESViewInterface | null;
   error: String | null;
 }
 
 const initialState: AuthState = {
+  status: '',
   employees_list: [],
   specific_employee_info: null,
   error: null,
@@ -28,30 +33,46 @@ export const employeesSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getEmployeesListSuccess, (state, action) => {
+        state.status = `${globalReducerSuccess}`;
         state.employees_list = action.payload.list;
         // state.employees_list = null;
         state.error = null;
       })
       .addCase(getEmployeesListFailure, (state, action) => {
+        state.status = `${globalReducerFailed}`
         state.employees_list = [];
         // state.employees_specific_info = null;
         state.error = action.payload;
       })
+      .addCase(getEmployeesListFailureCleanup, (state, action) => {
+        state.status = `${globalReducerRefreshed}`;
+        // state.employees_list = [];
+        // state.employees_specific_info = null;
+        state.error = null;
+      })
       .addCase(getSpecificEmployeeInfo, (state, action) => {
         // state.employees_list = null;
-
+        state.status = `${globalReducerLoading}`;
         state.specific_employee_info = null;
         state.error = null;
       })
       .addCase(getSpecificEmployeeInfoSuccess, (state, action) => {
         // state.employees_list = null;
+        state.status = `${globalReducerSuccess}`;
         state.specific_employee_info = action.payload.list;
         state.error = null;
       })
       .addCase(getSpecificEmployeeInfoFailure, (state, action) => {
         // state.employees_list = null;
+        state.status = `${globalReducerFailed}`
         state.specific_employee_info = null;
         state.error = action.payload;
+      })
+      .addCase(getSpecificEmployeeInfoFailureCleanup, (state, action) => {
+        state.status = `${globalReducerRefreshed}`;
+        // state.employees_list = [];
+        // state.employees_specific_info = null;
+        state.error = null;
       })
   },
 });
