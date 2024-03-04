bd174cd95670ec6f97f9ada4acededbf22fe0429
vargas <wilson.vargas05162001@gmail.com>
2024-03-04 20:11:34 +0800

    fix

--- proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/pvm-pagibig/local-component/right-side/pvm-pagibig-history.tsx
+++ proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/pvm-pagibig/local-component/right-side/pvm-pagibig-history.tsx
@@ -50,7 +50,7 @@ export default function PVMPAGIBIGPageHistory() {
         </Typography>
         </div>
       </div>
-      <div style={{ height: '600px', width: '100%' }}>
+      <div style={{ height: '90%', width: '100%' }}>
         <DataGrid
           rows={PAGIBIGViewData? PAGIBIGViewData as PAGIBIGViewInterface[]:[]}
           columns={PVMPAGIBIGPageColumns}
