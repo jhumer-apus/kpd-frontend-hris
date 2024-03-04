53e87702e6892a4b13c3c1d75b302b98ab3a3886
vargas <wilson.vargas05162001@gmail.com>
2024-03-04 20:22:44 +0800

    fix : bugs and improvements

--- proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/aa-asset-list/local-component/right-side/aa-asset-list-history.tsx
+++ proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/aa-asset-list/local-component/right-side/aa-asset-list-history.tsx
@@ -48,7 +48,7 @@ export default function AAASSETLISTPageHistory() {
         </Typography>
         </div>
       </div>
-      <div style={{ height: '600px', width: '100%' }}>
+      <div style={{ height: '90%', width: '100%' }}>
         <DataGrid
           rows={ASSETLISTViewData? ASSETLISTViewData as ASSETLISTViewInterface[]:[]}
           columns={AAASSETLISTPageColumns}
--- proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/ea-job-postings/local-component/right-side/ea-history-job-postings.tsx
+++ proto06/panels/src/pages/dashboard/admin-portal/first-inner-pages/second-inner-pages/ea-job-postings/local-component/right-side/ea-history-job-postings.tsx
@@ -49,7 +49,7 @@ export default function EAJOBPOSTINGSPageHistory() {
         </Typography>
         </div>
       </div>
-      <div style={{ height: '600px', width: '100%' }}>
+      <div style={{ height: '90%', width: '100%' }}>
         <DataGrid
           rows={JOBPOSTINGSViewData? JOBPOSTINGSViewData as JOBPOSTINGSViewInterface[]:[]}
           columns={EAJOBPOSTINGSPageColumns}
