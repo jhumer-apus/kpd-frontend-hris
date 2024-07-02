6a203b66e10ef17a2a5c545c5fe8d7aed3151954
jhumer-apus <jhumerapus@gmail.com>
2024-07-02 13:07:53 +0800

    1. Add label no holidays added

--- proto06/panels/src/pages/procedurals/holidays/local-components/list-of-holidays/list-of-holidays.tsx
+++ proto06/panels/src/pages/procedurals/holidays/local-components/list-of-holidays/list-of-holidays.tsx
@@ -51,46 +51,50 @@ function ListOfHolidaysComponent(props: HighlightedCalendarInterface) {
     }
     return (
         <div style={{height: '90%', overflowY: 'auto', padding: '6px'}}>
-        <ul className='flex flex-col items-center justify-center'>
-        {ListOfHolidaysState?.data && ListOfHolidaysState?.data?.map(({ id, holiday_date, holiday_description, holiday_type, holiday_location }, index) => (
-            <li style={{width: '95%'}} key={`${index}_li_list_holiday`}>
-                <Card 
-                    style={{marginTop: '20px', padding: '0px', cursor: 'pointer', transition: 'transform 0.3s ease', boxShadow: `2px 2px 7px ${holiday_type === 'SH'? HolidayColor._special_hex : HolidayColor._legal_hex}`}} 
-                    onMouseEnter={(e) => {
-                        e.currentTarget.style.transform = 'scale(1.05)';
-                    }}
-                    onMouseLeave={(e) => {
-                        e.currentTarget.style.transform = 'scale(1)';
-                    }}
-                    onClick={() => setValue(dayjs(holiday_date))}
-                >
-                    <CardHeader
-                        floated={false}
-                        variant="gradient"
-                        // color={b}
-                        className="absolute mt-4 grid h-16 w-16 place-items-center"
-                        style={{background: holiday_type === 'SH'? HolidayColor._special_hex : HolidayColor._legal_hex}}
-                        data-name={'iconwrap'}
+            <ul className='flex flex-col items-center justify-center'>
+            {ListOfHolidaysState?.data? ListOfHolidaysState?.data?.map(({ id, holiday_date, holiday_description, holiday_type, holiday_location }, index) => (
+                <li style={{width: '95%'}} key={`${index}_li_list_holiday`}>
+                    <Card 
+                        style={{marginTop: '20px', padding: '0px', cursor: 'pointer', transition: 'transform 0.3s ease', boxShadow: `2px 2px 7px ${holiday_type === 'SH'? HolidayColor._special_hex : HolidayColor._legal_hex}`}} 
+                        onMouseEnter={(e) => {
+                            e.currentTarget.style.transform = 'scale(1.05)';
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.style.transform = 'scale(1)';
+                        }}
+                        onClick={() => setValue(dayjs(holiday_date))}
                     >
-                        <span>{React.createElement( holiday_type === 'SH'? AvTimerOutlinedIcon : AccountBalanceOutlinedIcon , {className: 'w-6 h-6 text-white'})}</span>
-                    </CardHeader>
-                    <CardBody className="p-4 text-right">
-                        <Typography variant="body2" className="font-normal text-blue-gray-600">
-                        {holiday_description ? holiday_description + ' - ' : ''}{holiday_type === 'SH' ? 'Special Non-Working Holiday': 'Regular Holiday'}
-                        </Typography>
-                        <Typography variant="h4" color="blue-gray">
-                        {holiday_type == 'LH'? 'RH': 'SH'}
-                        </Typography>
-                    </CardBody>
-                    <CardFooter className="border-t flex justify-between border-blue-gray-50 p-4">
-                        <span>{dayjs(holiday_date).locale('en').format('MMMM D, YYYY')} - {`${holiday_location.charAt(0).toUpperCase()}${holiday_location.slice(1)}`}</span>
-                        <span title="Edit Holiday" onClick={() => {editHoliday(id)}} style={{zoom: 0.8}}>{React.createElement(EditCalendarOutlinedIcon, {style: {color: holiday_type === 'SH'? HolidayColor._special_hex: HolidayColor._legal_hex}})}</span>
-                    </CardFooter>
-                </Card>
-            </li>
-        ))}
-        {/* {ListOfHolidaysState?.data && "No Holidays to Generate"} */}
-        </ul>
+                        <CardHeader
+                            floated={false}
+                            variant="gradient"
+                            // color={b}
+                            className="absolute mt-4 grid h-16 w-16 place-items-center"
+                            style={{background: holiday_type === 'SH'? HolidayColor._special_hex : HolidayColor._legal_hex}}
+                            data-name={'iconwrap'}
+                        >
+                            <span>{React.createElement( holiday_type === 'SH'? AvTimerOutlinedIcon : AccountBalanceOutlinedIcon , {className: 'w-6 h-6 text-white'})}</span>
+                        </CardHeader>
+                        <CardBody className="p-4 text-right">
+                            <Typography variant="body2" className="font-normal text-blue-gray-600">
+                            {holiday_description ? holiday_description + ' - ' : ''}{holiday_type === 'SH' ? 'Special Non-Working Holiday': 'Regular Holiday'}
+                            </Typography>
+                            <Typography variant="h4" color="blue-gray">
+                            {holiday_type == 'LH'? 'RH': 'SH'}
+                            </Typography>
+                        </CardBody>
+                        <CardFooter className="border-t flex justify-between border-blue-gray-50 p-4">
+                            <span>{dayjs(holiday_date).locale('en').format('MMMM D, YYYY')} - {`${holiday_location.charAt(0).toUpperCase()}${holiday_location.slice(1)}`}</span>
+                            <span title="Edit Holiday" onClick={() => {editHoliday(id)}} style={{zoom: 0.8}}>{React.createElement(EditCalendarOutlinedIcon, {style: {color: holiday_type === 'SH'? HolidayColor._special_hex: HolidayColor._legal_hex}})}</span>
+                        </CardFooter>
+                    </Card>
+                </li>
+            )) : 
+                <div className='mt-4'>
+                    <Typography>No Holidays Added</Typography>
+                </div>
+            }
+            {/* {ListOfHolidaysState?.data && "No Holidays to Generate"} */}
+            </ul>
             <EditHolidayModal setIsOpenModal={setIsEdit} isOpenModal={isEdit} holidayId={holidayId}/>    
         </div>
     );
